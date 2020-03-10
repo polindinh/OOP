b8ba3b155de611ea4ccd6c41aef720ac22dac87d
polindinh <polin.dinh88@gmail.com>
2020-03-09 22:46:53 -0600

    added 3rd function to change to lower case

--- object.js
+++ object.js
@@ -1,5 +1,5 @@
 
-class KayneQuote {
+class KanyeQuote {
     constructor(quote) {
         this.kanyeWisdom = quote;
     }
@@ -7,17 +7,19 @@ class KayneQuote {
     methodOne() {
         //turn the whole quote to upper case
         var quote = this.kanyeWisdom.toUpperCase();
-        $("#content").html(quote);
+        $("#contentOne").html(quote);
     }
 
     methodTwo() {
         //change every second letter to capital
         var quote = this.kanyeWisdom.split('').map((letter, i) => i % 2 == 0 ? letter.toLowerCase() : letter.toUpperCase()).join('');
-        return  $("#content").html(quote);
+        return  $("#contentTwo").html(quote);
     
           }
     methodThree() {
-        //DO SOMETHING WITH THE QUOTE
+        //turn to lower case
+        var quote = this.kanyeWisdom.toLowerCase();
+        $("#contentThree").html(quote);
     }
 }
 // MAKE AJAX REQUEST TO GET QUOTE
@@ -25,7 +27,7 @@ class KayneQuote {
 // CALL YOUR THREE METHODS OF YOUR NEW OBJECT
 
 $(document).ready(function(){
-    $("#clickMe").click(function(){
+    $("#clickMeOne").click(function(){
         console.log('click');
         $.ajax({
             "url":"https://api.kanye.rest/",
@@ -33,7 +35,7 @@ $(document).ready(function(){
             "type":"GET",
             "dataType":"json",
             success: function(data){
-                var newQuote = new KayneQuote(data.quote);
+                var newQuote = new KanyeQuote(data.quote);
                 console.log(newQuote);
                 newQuote.methodOne();
             }
@@ -42,7 +44,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $("#clickMe1").click(function(){
+    $("#clickMeTwo").click(function(){
         console.log('click');
         $.ajax({
             "url":"https://api.kanye.rest/",
@@ -50,7 +52,7 @@ $(document).ready(function(){
             "type":"GET",
             "dataType":"json",
             success: function(data){
-                var newQuote = new KayneQuote(data.quote);
+                var newQuote = new KanyeQuote(data.quote);
                 console.log(newQuote);
                 newQuote.methodTwo();
             }
@@ -59,7 +61,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $("#clickMe2").click(function(){
+    $("#clickMeThree").click(function(){
         console.log('click');
         $.ajax({
             "url":"https://api.kanye.rest/",
@@ -67,10 +69,39 @@ $(document).ready(function(){
             "type":"GET",
             "dataType":"json",
             success: function(data){
-                var newQuote = new KayneQuote(data.quote);
+                var newQuote = new KanyeQuote(data.quote);
                 console.log(newQuote);
                 newQuote.methodThree();
             }
         });
     });
 });
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
